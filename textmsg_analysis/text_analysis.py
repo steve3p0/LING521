e343c482b2fa01520e357ebc95749e49e72cb86d
Steve Braich <33675736+steve3p0@users.noreply.github.com>
2019-11-12 07:07:38 -0800

    Connected graph.

--- textmsg_analysis/text_analysis.py
+++ textmsg_analysis/text_analysis.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
+from collections import defaultdict
 
 nltk.download('averaged_perceptron_tagger')
 nltk.download('universal_tagset')
@@ -43,96 +44,100 @@ messages = ['Gym?',
             'where r u???',
             'pinball']
 
+#print(f"Word Count: {map(len, map(nltk.word_tokenize, messages))}")
+#print(map(len, map(nltk.word_tokenize, messages)))
+
+sentence_lengths = [len(sentence.split()) for sentence in messages]
+total_words = sum(sentence_lengths)
+print(f"Sentence Lengths: {sentence_lengths}")
+print(f"Grand Total Words: {total_words}")
+
+insert_words = [ 'yeah', 'Ok', 'ahh' ]
 stops = set(stopwords.words("english"))
 
 counter_list = []
+words = defaultdict(list)
 for msg in messages:
     tokens = nltk.word_tokenize(msg)
     #print(tokens)
-    pos = nltk.pos_tag(tokens, tagset='universal')
-    print(f"\nmessage: {pos}")
+    word_tag_pairs = nltk.pos_tag(tokens, tagset='universal')
+    print(f"\nRaw Message: {msg}")
+    print(f"POS: {word_tag_pairs}")
 
+    for w, tag in word_tag_pairs:
+        words[tag].append(w)
 
     lstops = [x.lower() for x in stops]
     function_words = [word for word in tokens if word in lstops]
     fw_count = len(function_words)
-    print(f"Function Words: {fw_count}")
+    words['FW'].extend(function_words)
+    print(f"Function Words (Stop Words): {function_words}")
+    print(f"Function Word Count: {fw_count}")
 
-    count = Counter([j for i,j in pos])
+    count = Counter([j for i,j in word_tag_pairs])
+    count['FW'] = fw_count
     counter_list.append(count)
-    print(f"count: {count}")
+    print(f"POS Count: {count}")
 
 counter_pos = sum(counter_list, Counter())
+print(f"\nSummary - POS Tagging")
+print(f"Nouns: {words['NOUN']}")
+print(f"Verbs: {words['VERB']}")
+print(f"Adjectives: {words['ADJ']}")
+print(f"Adverbs: {words['ADV']}")
+print(f"Function Words: {words['FW']}")
+print(f"Inserts: {words['Inserts']}")
+
 print(f"\nCounts: {counter_pos}")
+print(f"Nouns: {counter_pos['NOUN']}")
+print(f"Verbs: {counter_pos['VERB']}")
+print(f"Adjectives: {counter_pos['ADJ']}")
+print(f"Adverbs: {counter_pos['ADV']}")
+print(f"Function Words: {counter_pos['FW']}")
+print(f"Inserts: {counter_pos['Inserts']}")
+
+# Counts
+#counter_static = Counter({'NOUN': 29, 'VERB': 12, 'ADP': 9, 'ADV': 8, 'PRON': 8, '.': 7, 'DET': 4, 'ADJ': 3, 'CONJ': 2})
+raw_counts_nouns = counter_pos['NOUN']
+raw_counts_verbs = counter_pos['VERB']
+raw_counts_adverbs = counter_pos['ADV']
+raw_counts_adjectives = counter_pos['ADJ']
+raw_counts_functors = counter_pos['FW']
+raw_counts_inserts = counter_pos['Inserts']
+
+percent_nouns = counter_pos['NOUN'] / total_words
+percent_verbs = counter_pos['VERB'] / total_words
+percent_adverbs = counter_pos['ADV'] / total_words
+percent_adjectives = counter_pos['ADJ'] / total_words
+percent_functors = counter_pos['FW'] / total_words
+percent_inserts = counter_pos['Inserts'] / total_words
+
+norm_counts_nouns = percent_nouns * 1000
+norm_counts_verbs = percent_verbs * 1000
+norm_counts_adverbs = percent_verbs * 1000
+norm_counts_adjectives = percent_adjectives * 1000
+norm_counts_functors = percent_functors * 1000
+norm_counts_inserts = percent_inserts * 1000
 
 # Graphing
-counter_static = Counter({'NOUN': 29, 'VERB': 12, 'ADP': 9, 'ADV': 8, 'PRON': 8, '.': 7, 'DET': 4, 'ADJ': 3, 'CONJ': 2})
-nouns = counter_static['NOUN']
-verbs = counter_static['VERB']
-adverbs = counter_static['ADV']
-adjectives = counter_static['ADJ']
+width = 0.7
+p1 = plt.bar(width=width, x=1, height=norm_counts_nouns)
+p2 = plt.bar(width=width, x=1, height=norm_counts_verbs, bottom=norm_counts_nouns)
+p3 = plt.bar(width=width, x=1, height=norm_counts_adverbs, bottom=norm_counts_nouns + norm_counts_verbs)
+p4 = plt.bar(width=width, x=1, height=norm_counts_adjectives, bottom=norm_counts_nouns + norm_counts_verbs + norm_counts_adverbs)
 
-N = 1
-ind = np.arange(N)    # the x locations for the groups
-width = 0.35
-
-p1 = plt.bar(ind, nouns, width, yerr=1)
-p2 = plt.bar(ind, verbs, width, bottom=nouns, yerr=1)
-p3 = plt.bar(ind, adverbs, width, bottom=nouns + verbs, yerr=1)
-p4 = plt.bar(ind, adjectives, width, bottom=nouns + verbs + adverbs, yerr=1)
-
-plt.ylabel('Frequency')
+plt.ylabel('Normed Word Counts Per 1000')
 plt.title('Distribution of Lexical Word Classes')
-plt.xticks(ind, ('Text Messages'))
-plt.yticks(np.arange(0, 81, 10))
-plt.legend((p1[0], p2[0], p3[0], p4[0]), ('Nouns', 'Verbs', 'Adverbs', 'Adjectives'))
+plt.title('2007 Text Messages: Frequency of Lexical Word Classes')
+plt.tick_params(
+    axis='x',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom=False,      # ticks along the bottom edge are off
+    top=False,         # ticks along the top edge are off
+    labelbottom=False) # labels along the bottom edge are off
 
+actual_last_value = norm_counts_nouns + norm_counts_verbs + norm_counts_adverbs
+max_y_value = total_words * 1000
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), handles=(p1[0], p2[0], p3[0], p4[0]), labels=('Nouns', 'Verbs', 'Adverbs', 'Adjectives'))
+plt.autoscale(False)
 plt.show()
-
-
-# N = 5
-# menMeans = (20, 35, 30, 35, 27)
-#
-# womenMeans = (25, 32, 34, 20, 25)
-# menStd = (2, 3, 4, 1, 2)
-# womenStd = (3, 5, 2, 3, 3)
-# ind = np.arange(N)    # the x locations for the groups
-# width = 0.35       # the width of the bars: can also be len(x) sequence
-#
-# p1 = plt.bar(ind, menMeans, width, yerr=menStd)
-# p2 = plt.bar(ind, womenMeans, width, bottom=menMeans, yerr=womenStd)
-#
-# plt.ylabel('Scores')
-# plt.title('Scores by group and gender')
-# plt.xticks(ind, ('G1', 'G2', 'G3', 'G4', 'G5'))
-# plt.yticks(np.arange(0, 81, 10))
-# plt.legend((p1[0], p2[0]), ('Men', 'Women'))
-#
-# plt.show()
-
-
-# N = 5
-# menMeans = (20, 35, 30, 35, 27)
-# womenMeans = (25, 32, 34, 20, 25)
-# menStd = (2, 3, 4, 1, 2)
-# womenStd = (3, 5, 2, 3, 3)
-# ind = np.arange(N)    # the x locations for the groups
-# width = 0.35       # the width of the bars: can also be len(x) sequence
-#
-# p1 = plt.bar(ind, menMeans, width, yerr=menStd)
-# p2 = plt.bar(ind, womenMeans, width,
-#              bottom=menMeans, yerr=womenStd)
-#
-# plt.ylabel('Scores')
-# plt.title('Scores by group and gender')
-# plt.xticks(ind, ('G1', 'G2', 'G3', 'G4', 'G5'))
-# plt.yticks(np.arange(0, 81, 10))
-# plt.legend((p1[0], p2[0]), ('Men', 'Women'))
-#
-# plt.show()
-
-
-
-
-
-
