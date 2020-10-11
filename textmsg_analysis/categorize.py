8f465db6ab2a899dc7231d76f1679ce3ce5140cd
Steve Braich <33675736+steve3p0@users.noreply.github.com>
2020-10-11 00:14:21 -0700

    updated insert word list.  added bonus question.

--- textmsg_analysis/categorize.py
+++ textmsg_analysis/categorize.py
@@ -1,61 +1,33 @@
 import nltk
 #from nltk.tag.simplify import simplify_wsj_tag
-#from nltk.corpus import brown
+from nltk.corpus import brown
+
+nltk.download()
 
 # Downloads
-nltk.download('universal_tagset')
+#nltk.download('universal_tagset')
 # nltk.download()
 #nltk.download('tagsets')
 # nltk.download('averaged_perceptron_tagger')
-messages = ['Gym?',
-            'yeah be there in about a half',
-            'Ok see you when you get here!',
-            'Seconds away',
-            'Meet me between smith and cramer asap',
-            'I got you and Taylor tix in pit section.',
-            'Get some milk please',
-            'Chk email',
-            'Made it',
-            'Do u know where u saved that movie on my compute',
-            'Im meeting some dude from the internet for happy hour ahh!',
-            'Wed is dinner for renetta call us soon',
-            'where r u???',
-            'pinball']
 
-#tag_set = nltk.help.upenn_tagset()
-#tagset_universal = nltk.map_tag()
-#tagset_universal = nltk.
+messages = ["'Your books have sold millions of copies,' the young interviewer was saying.",
+            'According to Kant and Laplace, the original mass of gas cooled and began to contract.',
+            "The minibar was filled with candy, mineral water, decaffeinated soft drinks and dairy products. " \
+            "'These are the kind of munchies which our research found helps sleep,' said Jeremy Baka, Hilton spokesman.",
+            'You guys can go to a whole bunch of places and you should not go to New Mexico.' ]
+
 tagset_upenn = nltk.help.upenn_tagset()
-#tagset_unvsl = nltk.tagset_mapping(="universal")
 print(tagset_upenn)
-#print(tagset_unvsl)
 
 pos_list = []
 for msg in messages:
     tokens = nltk.word_tokenize(msg)
-    pos = nltk.pos_tag(tokens, tagset='universal')
+    #pos = nltk.pos_tag(tokens, tagset='universal')
+    pos = nltk.pos_tag(tokens)
     #pos = tag_set.pos_tag(tokens, )
     #pos_simple = nltk.classify.
+    print(pos)
     pos_list.append(pos)
-    #print(pos)
 
-print(pos_list)
+#print(pos_list)
 
-# wordtags = nltk.ConditionalFreqDist((w.lower(), t)
-#         for w, t in nltk.corpus.brown.tagged_words(tagset="universal"))
-# #print(wordtags["report"])
-# #FreqDist({'NOUN': 135, 'VERB': 39})
-# kind = list(wordtags["kind"])
-
-class BackoffTagger:
-    def __init__(self):
-        self._taggers = [nltk.PerceptronTagger()]
-
-model = {'example_one': 'VB', 'example_two': 'NN'}
-tagger = nltk.tag.UnigramTagger(model=model, backoff=BackoffTagger())
-tagger.tag(['example_one'])
-
-
-
-print('the end')
-#['ADJ', 'NOUN']
